aeaebc54184050773a3f8d46a768466ccd942958
kyle <kylemagness123@gmail.com>
2023-02-05 01:35:25 -0500

    removed non command based stuff in robot.java
    Signed-off-by: kyle <kylemagness123@gmail.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,12 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -26,13 +26,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
-  //SparkMax variables
-  private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-  public SparkMaxPIDController m_pidController;
-  private RelativeEncoder m_encoder;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   /**
    * This function is run when the robot is first started up and should be used for any
--- src/main/java/frc/robot/commands/TeleopElevator.java
+++ src/main/java/frc/robot/commands/TeleopElevator.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class TeleopElevator extends CommandBase {
 private SparkMax sMax;
-private BooleanSupplier elevatorButton;
+// private BooleanSupplier elevatorButton;
 
     public TeleopElevator(SparkMax sMax, BooleanSupplier elevatorButton){
     this.sMax = sMax;
